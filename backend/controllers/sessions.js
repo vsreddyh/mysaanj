7571e2fc079e559d9c25e6c908515d8815d61a12
vsreddyh <shouryanreddyh@gmail.com>
2024-07-08 18:28:51 +0530

    session

--- backend/controllers/sessions.js
+++ backend/controllers/sessions.js
@@ -1,14 +1,11 @@
 const logincaretaker = (req, res) => {
     req.session.id="6687cc90dfb46fdd2abb5006"
-    res.json({id:"6687cc90dfb46fdd2abb5006"})
+    req.session.name="Vishnu"
+    req.session.type="Caretaker"
+    res.json({id:"6687cc90dfb46fdd2abb5006",name:"Vishnu",type:"Caretaker",type:"Caretaker"})
 }
 const checksession = (req, res) => {
-    if(req.session.id){
-        res.json(true)
-    }
-    else{
-        res.json(false)
-    }
+    res.json(req.session.type)
 }
 module.exports = {
     logincaretaker,
