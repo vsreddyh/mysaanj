ceb03f3c08f46ed875bb3516ae65a1f80855f1b9
vsreddyh <shouryanreddyh@gmail.com>
2024-07-08 18:56:11 +0530

    session1

--- backend/controllers/sessions.js
+++ backend/controllers/sessions.js
@@ -5,7 +5,7 @@ const logincaretaker = (req, res) => {
     res.json({id:"6687cc90dfb46fdd2abb5006",name:"Vishnu",type:"Caretaker",type:"Caretaker"})
 }
 const checksession = (req, res) => {
-    res.json(req.session.type)
+    res.json({id:req.session.id,name:req.session.name,type:req.session.type})
 }
 module.exports = {
     logincaretaker,
